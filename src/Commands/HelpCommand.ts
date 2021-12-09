ad222a0c9f1aed8c9fa45dcf56d297ee0d5940be
Roberto Rebello <roberto.rebello@gmail.com>
2021-12-09 20:43:01 -0300

    Fixing a very difficult bug (#3)

--- src/Commands/HelpCommand.ts
+++ src/Commands/HelpCommand.ts
@@ -9,9 +9,9 @@ const helpMessage =
   ' This must be set in the future. On the day of the session at 10:30 Telemarvin will send a reminder together with a poll.\n' +
   '- <b>nextSession</b>: Prints the earliest next session date. \n' +
   '- <b>deleteSession</b>: <i>dd/mm/yy</i>: Deletes the session at the given date. \n' +
-  '- <b>instantPool</b>: Creates a sample session hour pool, for those spontaneous sessions.' +
-  '- <b>weekPool</b>: Creates a sample week pool, for those hard to schedule weeks.' +
-  ' \n\n' +
+  '- <b>instantPool</b>: Creates a sample session hour pool, for those spontaneous sessions. \n' +
+  '- <b>weekPool</b>: Creates a sample week pool, for those hard to schedule weeks. \n' +
+  ' \n' +
   'Are you missing any feature? Feel free to open an issue or even a PR on ' +
   '<a href="https://github.com/Hamitay/TeleMarvin/">TeleMarvin\'s repo @ Github</a>'
 
