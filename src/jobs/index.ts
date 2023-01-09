d052e7a9543e767f0380af49bd235cf241b64abf
Hamitay <henriqueamitay@gmail.com>
2023-01-08 22:24:39 -0300

    Exception handling on jobs

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -1,16 +1,25 @@
-import { session, Telegraf } from 'telegraf';
-import { CronJob } from 'cron';
-import { SessionService } from '../services/SessionService';
-import { parseSQLDateToString } from '../utils/dateUtils';
+import { session, Telegraf } from "telegraf";
+import { CronJob } from "cron";
+import { SessionService } from "../services/SessionService";
+import { parseSQLDateToString } from "../utils/dateUtils";
 
-import messages from './messages';
-import { WeeklySessionService } from '../services/WeeklySessionService';
+import messages from "./messages";
+import { WeeklySessionService } from "../services/WeeklySessionService";
 
-const TIMEZONE = 'America/Sao_Paulo';
-const NEXT_SESSION_OPTIONS = ['18h', '18h30', '19h', '19h30', '20h', '20h30', '21h', 'Other'];
+const TIMEZONE = "America/Sao_Paulo";
+const NEXT_SESSION_OPTIONS = [
+  "18h",
+  "18h30",
+  "19h",
+  "19h30",
+  "20h",
+  "20h30",
+  "21h",
+  "Other",
+];
 
-const NEXT_SESSION_CRON = '0 30 10 * * * ';
-const NEXT_WEEKLYSESSION_CRON = '0 31 10 * * * ';
+const NEXT_SESSION_CRON = "0 30 10 * * * ";
+const NEXT_WEEKLYSESSION_CRON = "0 31 10 * * * ";
 
 export class Jobs {
   #bot: Telegraf;
@@ -40,40 +49,51 @@ export class Jobs {
       null,
       true,
       TIMEZONE
-    )
+    );
 
     const stopJobs = () => {
-      console.info('Stopping cron jobs');
+      console.info("Stopping cron jobs");
       job.stop();
       weeklySessionJob.stop();
     };
 
     // Stop jobs on process exit
-    process.once('SIGINT', stopJobs);
-    process.once('SIGTERM', stopJobs);
+    process.once("SIGINT", stopJobs);
+    process.once("SIGTERM", stopJobs);
   }
 
   async nextWeeklySession(): Promise<void> {
-    const sessions = await this.#weeklySessionService.getWeeklySessionsOfTheDay();
+    const sessions =
+      await this.#weeklySessionService.getWeeklySessionsOfTheDay();
     sessions.forEach((session) => {
-      this.#bot.telegram.sendPoll(
-        session.groupId,
-        messages.WEEKLY_POLL_MESSAGE(session.dow),
-        NEXT_SESSION_OPTIONS,
-        { is_anonymous: false, allows_multiple_answers: true }
-      )
-    })
+      try {
+        this.#bot.telegram.sendPoll(
+          session.groupId,
+          messages.WEEKLY_POLL_MESSAGE(session.dow),
+          NEXT_SESSION_OPTIONS,
+          { is_anonymous: false, allows_multiple_answers: true }
+        );
+      } catch (error) {
+        console.error("Error sending pool for weekly session");
+        console.error(error);
+      }
+    });
   }
 
   async nextSessionJob(): Promise<void> {
     const sessions = await this.#sessionService.getTodaysSession();
     sessions.forEach((session) => {
-      this.#bot.telegram.sendPoll(
-        session.groupId,
-        messages.POLL_MESSAGE(parseSQLDateToString(session.date)),
-        NEXT_SESSION_OPTIONS,
-        { is_anonymous: false, allows_multiple_answers: true }
-      );
+      try {
+        this.#bot.telegram.sendPoll(
+          session.groupId,
+          messages.POLL_MESSAGE(parseSQLDateToString(session.date)),
+          NEXT_SESSION_OPTIONS,
+          { is_anonymous: false, allows_multiple_answers: true }
+        );
+      } catch (error) {
+        console.error("Error sending pool for session");
+        console.error(error);
+      }
     });
   }
 }
